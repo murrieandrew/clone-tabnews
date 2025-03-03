820cd876f4eb195ebb99cbcaadbc21c3c3c93da9
Murrie Andrew <murrieandrew@gmail.com>
2025-03-03 17:01:54 +0000

    atualizacao do texto h1 da home

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home () {
-    return <h1>Em breve Lançamento do Website APP Novo Coração.🫀</h1>
+    return <h1>Oi Princesa, aperte os cintos, pq breve será o Lançamento do Website APP Novo Coração.🫀</h1>
 } 
 export default Home;
